6b1a75fdb55a838e9c8475cea00e583a1c0845c9
baileydodds <ddodds@chicagobooth.edu>
2025-05-18 17:12:39 +0000

    1

--- app.rb
+++ app.rb
@@ -7,3 +7,10 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/process_roll") do
+  "
+  <h1>Welcome to your Sinatra App!</h1>
+  <p>Define some routes in app.rb</p>
+  "
+end
