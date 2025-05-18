367325b91f78faf014017e842fd9b0e6704995e9
baileydodds <ddodds@chicagobooth.edu>
2025-05-18 17:12:44 +0000

    2

--- app.rb
+++ app.rb
@@ -3,14 +3,21 @@ require "sinatra/reloader"
 
 get("/") do
   "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
+  <h1>Dice Roll</h1>
   "
+  erb(:home)
 end
 
 get("/process_roll") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  @num_dice = params.fetch("dice").to_i
+  
+  @num_sides = params.fetch("sides").to_i
+
+  @rolls = []
+
+  @num_dice.times do
+    @rolls.push(rand(1..@num_sides))
+  end
+
+  erb(:results)
 end
